f2423fa0dc4c1c4ade1fb8f52901c9fc34e4bfea
Marcel Zielinski <marziel2001@gmail.com>
2026-04-19 19:46:39 +0200

    Refactor

--- scripts/count_metrics.py
+++ scripts/count_metrics.py
@@ -14,11 +14,11 @@ def calculate_metrics_from_text(ref_text: str, hyp_text: str) -> dict[str, float
 	}
 
 
-def calculate_metrics(ref_path: str, hyp_path: str) -> dict[str, float]:
+def calculate_metrics_from_file(ref_path: str, hyp_path: str) -> dict[str, float]:
 	ref_text = read_file(ref_path)
 	hyp_text = read_file(hyp_path)
 	return calculate_metrics_from_text(ref_text, hyp_text)
 
 
 if __name__ == "__main__":
-	calculate_metrics(sys.argv[1], sys.argv[2])
+	calculate_metrics_from_file(sys.argv[1], sys.argv[2])
